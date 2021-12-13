e17a19056556e640082ab33f970524d22bd7f5d7
xlg_2021 <xlglovely@gmail.com>
2021-12-13 23:18:20 +0800

    add baidu_api_test.py to repository

---
+++ baidu_api_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  5 19:07:43 2021
+
+@author: xin
+"""
+import requests
+import base64
+import pandas as pd
+import numpy as np
+import os
+import datetime
+import time
+import docx
+import win32com
+import json
+import sqlalchemy
+import pymysql
+import neo4j
+import chardet
+import pdfplumber
+import re
+import pytesseract
+import PIL
+
+import beautifulsoup4	#bs4，导入命令可能有误
